beb2c209f8e7e2aa7c0f8473c3b178e96710395d
Pr3Nom <mchlgirard@gmail.com>
2022-10-09 00:17:36 +0200

    create msg

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
-	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
+	alice = "cosmos1pzcza54ljnklvayxl9q4g8w38x3yz9jd56n3a7"
+	bob   = "cosmos1he3t9mgecp5r8vgmcsf2jsgqmlrzpuanqmlcgw"
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
 
