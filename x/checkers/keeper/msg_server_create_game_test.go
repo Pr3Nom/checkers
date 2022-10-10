1578e5d349169aabcb407753072a82c3b206a1cc
pr3nom <mchlgirard@gmail.com>
2022-10-11 00:02:59 +0200

    create & save game - unit test

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"testing"
 
-	keepertest "github.com/b9lab/checkers/testutil/keeper"
+	keepertest "github.com/Pr3Nom/checkers/testutil/keeper"
+	"github.com/Pr3Nom/checkers/x/checkers"
 	"github.com/Pr3Nom/checkers/x/checkers/keeper"
 	"github.com/Pr3Nom/checkers/x/checkers/types"
 	"github.com/stretchr/testify/require"
@@ -17,6 +18,13 @@ const (
 	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
+
 func TestCreateGame(t *testing.T) {
 	msgServer, context := setupMsgServer(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
@@ -26,11 +34,7 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", // TODO: update with a proper value when updated
+		GameIndex: "1", 
 	}, *createResponse)
 }
 
-func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-	k, ctx := keepertest.CheckersKeeper(t)
-	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
-}
