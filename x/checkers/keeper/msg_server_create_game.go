d2cd0319786befeb958eee1cf3ab6282cd003019
pr3nom <mchlgirard@gmail.com>
2022-10-10 23:41:19 +0200

    create & save game

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -4,14 +4,37 @@ import (
 	"context"
 
 	"github.com/Pr3Nom/checkers/x/checkers/types"
+	"github.com/Pr3Nom/checkers/x/checkers/rules"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found")
+	}
 
-	return &types.MsgCreateGameResponse{}, nil
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+	    Index: newIndex,
+	    Board: newGame.String(),
+	    Turn:  rules.PieceStrings[newGame.Turn],
+	    Black: msg.Black,
+	    Red:   msg.Red,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+	    return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{
+	    GameIndex: newIndex
+	}, nil
 }
